a2f00798a0f24f2182ff7e6e0737dd9419cef4b7
Steven Quesada <stevenq161198@gmail.com>
2019-11-19 21:37:46 -0600

    Create a canvas

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, NgZone } from '@angular/core';
+import { Square } from './square';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <canvas #canvas width="900" height="500"></canvas>
+    <button (click)="play()">Play</button>
+  `,
+  styles: ['canvas { border-style: solid; margin-top: 25px; margin-left: 50px}']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
+  requestId;
+  interval;
+  squares: Square[] = [];
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.fillStyle = 'red';
+    this.ngZone.runOutsideAngular(() => this.tick());
+    setInterval(() => {
+      this.tick();
+    }, 200);
+  }
+
+  tick() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.squares.forEach((square: Square) => {
+      square.moveRight();
+    });
+    this.requestId = requestAnimationFrame(() => this.tick);
+  }
+
+  play() {
+    const square = new Square(this.ctx);
+    this.squares = this.squares.concat(square);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    cancelAnimationFrame(this.requestId);
+  }
 }
