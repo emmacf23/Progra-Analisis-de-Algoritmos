2b0d1cbbb4297c68760f11ca93ee671c3a6f7eb6
Steven Quesada <stevenq161198@gmail.com>
2019-11-20 20:43:07 -0600

    gitflow-feature-stash: UpdatedTreesandServices

--- Client/src/app/services/tests.service.ts
+++ Client/src/app/services/tests.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import {Tree} from './tree'
 @Injectable({
   providedIn: 'root'
 })
 export class TestsService {
 
+  tree: Object;
+
+  trees: Tree[];
   serverData: JSON;
   constructor(private http: HttpClient) { }
 
@@ -15,10 +18,26 @@ export class TestsService {
     this.request()
       .subscribe((response) => {
         this.serverData = response as JSON
+        this.readData(response.trees);
         console.log(this.serverData);
-      });
-  }
+        console.log(this.trees);
 
+      });
+      
+  }
+  getGrowPercentage(pTreeLength, pTreeLevels, pLeafLength){
+    return (pLeafLength / pTreeLength) ** 1 / pTreeLevels;
+  }
+  readData(pTrees){
+    this.trees = []
+    for (var i in pTrees){
+      var leafLength = pTrees[i].leafLength
+      var length = pTrees[i].length
+      var levels = pTrees[i].levels
+      var posX = pTrees[i].posX
+      this.trees.push(new Tree(length,this.getGrowPercentage(length,levels,leafLength),posX,levels))
+    }
+  }
   request(): Observable<any> {
     return this.http.post<any>("/run/", {
         // name: "Comase esta"
@@ -29,6 +48,4 @@ export class TestsService {
       // catchError(this.handleError('addHero', hero))
     );
   }
-
-
 }
